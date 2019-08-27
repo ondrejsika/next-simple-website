3744385f0fe8457f3bd7dde2ec0eb252ee6a4343
Ondrej Sika <ondrej@ondrejsika.com>
2019-08-27 11:19:28 +0200

    add footer & redesign header

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Head from "next/head";
 import App from "next/app";
+import { networkInterfaces } from "os";
 
 class MyApp extends App {
   render() {
@@ -13,20 +14,32 @@ class MyApp extends App {
           <title>Ondrej Sika</title>
         </Head>
         <p>
-          <h3 className="site-header">Ondrej Sika</h3> ~{" "}
+          <h3 className="site-header">Ondrej Sika</h3>
           <Link href="/">
             <a>About Me</a>
           </Link>{" "}
-          ~{" "}
+          /{" "}
           <Link href="/contact-me">
             <a>Contact Me</a>
           </Link>
         </p>
         <Component {...pageProps} />
+        <p className="footer">
+          <b>
+            &copy; Ondrej Sika / Source code on{" "}
+            <a href="https://github.com/ondrejsika/next-simple-website">
+              Github
+            </a>
+          </b>
+        </p>
         <style jsx>{`
           .site-header {
             display: inline;
             font-size: 1em;
+            margin-right: 20px;
+          }
+          .footer {
+            font-size: 0.7em;
           }
         `}</style>
         <style jsx global>{`
